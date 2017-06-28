c703ca324b0c47c65d0a0bc25f31f3bb2f0cae7d
z <2091469944@qq.com>
2017-06-28 22:11:34 +0800

    first day end

--- testphp5.php
+++ testphp5.php
@@ -20,6 +20,30 @@
 	}
 	$person=new Person("Andi");
 	changeName($person,"Stig");
+	//php4 中 输出 Andi
 	print $person->getName();
 
+
+	final class FinalClass{
+
+	}
+	//final 不能被继承 会报错
+	// class BogusClass extends FinalClass{
+	// }
+	echo "<br>";
+	class MyClass{
+		function __clone(){
+			print "Object is being cloned";
+		}
+	}
+	$obj = new MyClass();
+	$obj_copy = clone $obj;
+	// $obj_copy2 = clone $obj_copy;
+
+	echo "<br>";
+	class MyClass2 {
+		const SUCCESS = "Success";
+		const FAILUER = "Failure";
+	}
+	print MyClass2::SUCCESS;
 ?>
