dee865f7dd46b70af20610e6f73021d09687caed
z <2091469944@qq.com>
2017-06-28 21:53:57 +0800

    php 4 Andi

---
+++ testphp5.php
@@ -0,0 +1,25 @@
+﻿<?php
+	class Person{
+		var $name;
+		function getName()
+		{
+			return $this->name;
+		}
+		function setName($name)
+		{
+			$this->name=$name;
+		}
+		function Person($naem)
+		{
+			$this->setName($name);
+		}
+	}
+	function changeName($person,$name)
+	{
+		$person->setName($name);
+	}
+	$person=new Person("Andi");
+	changeName($person,"Stig");
+	print $person->getName();
+
+?>
