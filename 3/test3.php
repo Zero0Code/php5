a99773194ca9c8a973e2a27c9f1a92bb92c527b8
z <2091469944@qq.com>
2017-07-18 20:23:34 +0800

    .

--- 3/test3.php
+++ 3/test3.php
@@ -69,4 +69,123 @@
 	$obj = new MyColorEnumClass();
 	echo "<br>";
 	$obj->printBlue();
+
+
+	class MyClass3
+	{
+		public $var = 1;
+	}
+
+	$obj1 = new MyClass3();
+	$obj2 = $obj1;
+	$obj2->var = 2;
+	echo "<br>";
+	print $obj1->var;
+	//PHP4 $obj1->var=1 PHP5 $obj1->var=2;
+
+//克隆 	
+	$obj1 = new MyClass3();
+	$obj2 = clone $obj1;
+	$obj2->var = 2;
+	echo "<br>";
+	print $obj1->var;
+//克隆 	END
+
+
+//多态
+	class Cat
+	{
+		function miau()
+		{
+			print "miau";
+		}
+	}
+
+	class Dog
+	{
+		function wuff()
+		{
+			print "wufff";
+		}
+	}
+
+	function printTheRightSound($obj)
+	{
+		if ($obj instanceof Cat) {
+			$obj->miau();
+		} else if ($obj instanceof Dog) {
+			$obj->wuff();
+		} else {
+			print "Error: Passed wrong kind of object";
+		}
+			print "\n";
+	}
+	echo "<br>";
+	printTheRightSound(new Cat());
+	echo "<br>";
+	printTheRightSound(new Dog());
+	//上面这个例子不可扩展
+
+	//下面例子可扩展
+	class Animal
+	{
+		public function makeSound()
+		{
+			print "Error: This method should be re-implemented in the children";
+		}
+	}
+
+	class Cat2 extends Animal 
+	{
+		public function makeSound()
+		{
+			print "miau";
+		}
+	}
+
+	class Dog2 extends Animal
+	{
+		public function makeSound()
+		{
+			print "wuff";
+		}
+	}
+
+	function printTheRightSound2($obj)
+	{
+		if ($obj instanceof Animal) {
+			$obj->makeSound();
+		} else {
+			print "Error: Passed wrong kind of object";
+		}
+		print "\n";
+	}
+	echo "<br>";
+	printTheRightSound2(new Cat2());
+	echo "<br>";
+	printTheRightSound2(new Dog2());
+//多态END
+
+//parent:: 父类 self:: 子类
+	class Ancestor
+	{
+		const NAME = "Ancestor";
+		function __construct()
+		{
+			print "In" . self::NAME . "constructior\n";
+		}
+	}
+
+	class Child extends Ancestor
+	{
+		const NAME = "Child";
+		function __construct()
+		{
+			parent::__construct();
+			print "In" . self::NAME . "constructior\n";
+		}
+	}
+	echo "<br>";
+	$obj = new Child();
+//parent:: 父类 self:: 子类
 ?>
